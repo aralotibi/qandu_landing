69c6cea5df5640e476b68041777515c24a548f1a
Abdullah <aralotibi@quinnipiac.com>
2015-09-22 22:42:59 +0000

    Add success page

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,7 @@ from .views import *
 urlpatterns = patterns('',
     url(r'^$', Home.as_view(), name='home'),
     url(r'^question/create/$', QuestionCreateView.as_view(), name='question_create'),
+    url(r'^success/$', Success.as_view(), name='success'),
+
 
 )
